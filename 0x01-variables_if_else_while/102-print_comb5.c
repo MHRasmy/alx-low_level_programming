902fed42b59c59bdba47656c07e7318cd87bbe70
MHRasmy <18063@stemegypt.edu.eg>
2023-02-17 13:06:28 -0800

    bash commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - random number to the variable n
+ * compare n to zero and return n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 100; i++)
+	{
+		for (j = 1; j <= 100; j++)
+		{
+			if (i == j)
+				continue;
+			putchar(i / 10 + '0');
+            		putchar(i % 10 + '0');
+            		putchar(' ');
+            		putchar(j / 10 + '0');
+            		putchar(j % 10 + '0');
+            		if (i == 98 && j == 99) {
+                		continue;
+            		}
+            		putchar(',');
+            		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
