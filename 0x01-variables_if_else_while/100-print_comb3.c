bb1d8157fb59ed541a200677ade29aab1151a127
MHRasmy <18063@stemegypt.edu.eg>
2023-02-17 12:41:56 -0800

    bash commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,16 +13,19 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 8; i++)
 	{
-		for (j = 1; j < 10; j++)
+		for (j = 1; j <= 9; j++)
 		{
-			putchar('0' + i);
-			putchar('0' + j);
-			if (i != 9 || j != 9)
+			if (i != j && i < j)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar('0' + i);
+				putchar('0' + j);
+				if (i != 8 || j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
