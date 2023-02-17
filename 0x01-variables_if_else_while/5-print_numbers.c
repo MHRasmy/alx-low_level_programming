cd20aecbcb88f3cebacaaa052277280c95a5e50a
MHRasmy <18063@stemegypt.edu.eg>
2023-02-17 10:21:43 -0800

    bash commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - random number to the variable n
+ * compare n to zero and return n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		printf("%d", c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
