d0a68677782b15ad0671dc5c03fb9212be6689eb
MHRasmy <18063@stemegypt.edu.eg>
2023-02-16 16:54:28 -0800

    bash commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	printf("The size of int is %ld bytes\n", sizeof(int));
-	printf("The size of short is %ld bytes\n", sizeof(short));
-	printf("The size of long is %ld bytes\n", sizeof(long));
-	printf("The size of long long is %ld bytes\n", sizeof(long long));
-	printf("The size of float is %ld bytes\n", sizeof(float));
-	printf("The size of double is %ld bytes\n", sizeof(double));
-	printf("The size of long double is %ld bytes\n", sizeof(long double));
-	printf("The size of char is %ld bytes\n", sizeof(char));
-	printf("The size of void is %ld bytes\n", sizeof(void));
+	printf("Size of int is %ld bytes\n", sizeof(int));
+	printf("Size of short is %ld bytes\n", sizeof(short));
+	printf("Size of long is %ld bytes\n", sizeof(long));
+	printf("Size of long long is %ld bytes\n", sizeof(long long));
+	printf("Size of float is %ld bytes\n", sizeof(float));
+	printf("Size of double is %ld bytes\n", sizeof(double));
+	printf("Size of long double is %ld bytes\n", sizeof(long double));
+	printf("Size of char is %ld bytes\n", sizeof(char));
+	printf("Size of void is %ld bytes\n", sizeof(void));
 	return (0);
 }
