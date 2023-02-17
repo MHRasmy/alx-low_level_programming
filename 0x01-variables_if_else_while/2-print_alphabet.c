21cf056d784ff1cd381a7ee3a15af847a775d0fa
MHRasmy <18063@stemegypt.edu.eg>
2023-02-17 10:10:13 -0800

    bash commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - random number to the variable n
+ * compare n to zero and return n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
