a3b9b220e8611719c9acfde9f0754cbf5c8b1a0d
MHRasmy <18063@stemegypt.edu.eg>
2023-02-16 16:58:35 -0800

    bash commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,10 @@
 
 int main(void)
 {
-	printf("Size of int is %ld bytes\n", sizeof(int));
-	printf("Size of short is %ld bytes\n", sizeof(short));
-	printf("Size of long is %ld bytes\n", sizeof(long));
-	printf("Size of long long is %ld bytes\n", sizeof(long long));
-	printf("Size of float is %ld bytes\n", sizeof(float));
-	printf("Size of double is %ld bytes\n", sizeof(double));
-	printf("Size of long double is %ld bytes\n", sizeof(long double));
-	printf("Size of char is %ld bytes\n", sizeof(char));
-	printf("Size of void is %ld bytes\n", sizeof(void));
+	printf("Size of a char: %ld bytes\n", sizeof(char));
+	printf("Size of an int: %ld bytes\n", sizeof(int));
+	printf("Size of a long int: %ld bytes\n", sizeof(long));
+	printf("Size of a long long int: %ld bytes\n", sizeof(long long));
+	printf("Size of a float: %ld bytes\n", sizeof(float));
 	return (0);
 }
