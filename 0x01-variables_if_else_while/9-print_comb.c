3a76c71515e3553d1e5275f5fe4303c7e8f95de2
MHRasmy <18063@stemegypt.edu.eg>
2023-02-17 12:11:22 -0800

    bash commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - random number to the variable n
+ * compare n to zero and return n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar('0' + c);
+		if (c != 9)
+			putchar(',');
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
