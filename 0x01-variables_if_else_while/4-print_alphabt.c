08da1a65b034c11509010373fbebc23c69af96de
MHRasmy <18063@stemegypt.edu.eg>
2023-02-17 10:18:24 -0800

    bash commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - random number to the variable n
+ * compare n to zero and return n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
