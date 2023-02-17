8c5d4782114a99272463a08bdb316e5ca18b6475
MHRasmy <18063@stemegypt.edu.eg>
2023-02-17 13:14:43 -0800

    bash commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,18 +15,19 @@ int main(void)
 
 	for (i = 0; i <= 98; i++)
 	{
-		for (j = 1; j <= 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar(i / 10 + '0');
-            		putchar(i % 10 + '0');
-            		putchar(' ');
-            		putchar(j / 10 + '0');
-            		putchar(j % 10 + '0');
-            		if (i == 98 && j == 99) {
-                		continue;
-            		}
-            		putchar(',');
-            		putchar(' ');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i == 98 && j == 99)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
