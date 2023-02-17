b6bca29420ace13b76bf44873a0ad7fcb6010294
MHRasmy <18063@stemegypt.edu.eg>
2023-02-17 12:05:38 -0800

    bash commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - random number to the variable n
+ * compare n to zero and return n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		if (c < 10)
+			putchar('0' + i);
+		else
+			putchar('a' + i - 10);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
