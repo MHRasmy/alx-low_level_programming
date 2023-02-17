285b02f435e22b535ac33a036fa6b8eea8912448
MHRasmy <18063@stemegypt.edu.eg>
2023-02-16 17:25:09 -0800

    bash commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	int n = sizeof(s) / sizeof(s[0]);
 
@@ -18,5 +18,6 @@ int main(void)
 	{
 		putchar(s[i]);
 	}
+	putchar('\n');
 	return (0);
 }
