2e109dfa1f9aa819fe66996df94a4c11865d2cc0
MHRasmy <18063@stemegypt.edu.eg>
2023-02-23 17:59:16 -0800

    bash commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 
 	/**
@@ -16,6 +19,8 @@ int main(void)
 	*	putchar(i);
 	* }
 	*/
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
