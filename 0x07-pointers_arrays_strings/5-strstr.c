9cfa6094b088db3966e0b52ee61af4750a9f2096
MHRasmy <18063@stemegypt.edu.eg>
2023-03-06 18:13:45 -0800

    bash commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,18 +7,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-        for (; *haystack != '\0'; haystack++)
-        {
-                char *l = haystack;
-                char *p = needle;
-                while (*l == *p && *p != '\0')
-                {
-                        l++;
-                        p++;
-                }
-                if (*p == '\0')
-                        return (haystack);
-        }
-        return (0);
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+		
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
 }
 
---
+++ 0x07-pointers_arrays_strings/i
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in {5..7}
+do
+vi $i*
+done
