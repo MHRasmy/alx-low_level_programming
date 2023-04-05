485eb21b9ba565e4f275f00e31fcfe5204543d62
MHRasmy <18063@stemegypt.edu.eg>
2023-04-04 18:37:34 -0700

    implementing 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pop_listint - frees a listint_t list, return head data
+ * @head: Pointer to pointer listint_t
+ *
+ * Return: head data, 0 if empty 
+ */
+int pop_listint(listint_t **head)
+{
+	int n = 0;
+	listint_t *curr;
+
+	if ((*head) == NULL)
+		return (n);
+	else
+	{
+		curr = (*head);
+		(*head) = (*head)->next;
+		n = curr->n;
+
+		free(curr);
+	}
+
+	return (n);
+}
