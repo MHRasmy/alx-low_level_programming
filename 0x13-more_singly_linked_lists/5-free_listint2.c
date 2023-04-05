368fac5f78a78ee09c21c23b5df4947a7c513279
MHRasmy <18063@stemegypt.edu.eg>
2023-04-04 18:18:51 -0700

    implementing 4-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list, set head to NULL
+ * @head: Pointer to pointer listint_t
+ *
+ * Return: address of the new element, NULL if failed
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *curr;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		curr = *head;
+		(*head) = (*head)->next;
+		free(curr);
+	}
+
+	(*head) = NULL;
+}
