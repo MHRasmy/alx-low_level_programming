c373841d93ca4b874742b578b9c3dffea13f4cbe
MHRasmy <18063@stemegypt.edu.eg>
2023-04-04 19:24:24 -0700

    implementing 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_listint - sum of all the data (n) of a listint_t
+ * @head: pointer to the head of the list
+ *
+ * Return: sum, 0 if empty 
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *curr = head;
+
+	while (curr)
+	{
+		sum += curr->n;
+		curr = curr->next;
+	}
+
+	return (sum);
+}
