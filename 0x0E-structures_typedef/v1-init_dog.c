49fa6eeee00437c60a6137538d61b59260df518f
MHRasmy <18063@stemegypt.edu.eg>
2023-04-05 17:48:52 -0700

    implementing 1-init_dog.c

---
+++ 0x0E-structures_typedef/v1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initializes a struct dog
+ * @d: Pointer to the struct dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    if (d != NULL)
+    {
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+    }
+}
