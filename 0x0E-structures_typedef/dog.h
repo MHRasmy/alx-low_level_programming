a5354ed8c5f960216de194f93365558d7075ffe2
MHRasmy <18063@stemegypt.edu.eg>
2023-04-05 18:18:00 -0700

    defining typedef struct dog dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,12 @@
  * Description: structure called dog containing name, age and owner's name
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* DOG_H */
