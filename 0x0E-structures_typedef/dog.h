9c94d56d87d20f066089d9ed3d8cde89e1f52e12
MHRasmy <18063@stemegypt.edu.eg>
2023-04-05 17:42:51 -0700

    implementing dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog's information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ *
+ * Description: structure called dog containing name, age and owner's name
+ */
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
