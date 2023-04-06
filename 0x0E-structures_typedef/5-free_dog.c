178a6db5966a5e3a97b4a6e9de41be714ea3e3b9
MHRasmy <18063@stemegypt.edu.eg>
2023-04-05 18:27:31 -0700

    implementing 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to struct dog to be freed
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
