3f950ebb8219aea1a15529b8a2264360c5110e16
MHRasmy <18063@stemegypt.edu.eg>
2023-04-08 00:09:16 -0700

    removed usless var

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,11 +7,9 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int bit = 1;
-
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	*n |= (bit << index);
+	*n |= (1 << index);
 	return (1);
 }
