894dd8e1acf296e364020ac0cc34f56ec99d1958
MHRasmy <18063@stemegypt.edu.eg>
2023-04-17 02:09:57 -0700

    implementing 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,39 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+
 /**
- * main - prints the minimum number of coins to
- * make change for an amount of money
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - multiplies two numbers.
+ * @argc: argument count
+ * @argv: arguments
  *
- * Return: 0 (Success), 1 (Error)
+ * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int num, j, result;
-	int coins[] = {25, 10, 5, 2, 1};
+	int cents, coins = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	num = atoi(argv[1]);
-	result = 0;
-	if (num < 0)
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
 	{
-		printf("0\n");
+		printf("%d\n", 1);
 		return (0);
 	}
-	for (j = 0; j < 5 && num >= 0; j++)
-	{
-		while (num >= coins[j])
-		{
-			result++;
-			num -= coins[j];
-		}
-	}
-	printf("%d\n", result);
+
+	coins += cents / 25;
+	cents %= 25;
+	coins += cents / 10;
+	cents %= 10;
+	coins += cents / 5;
+	cents %= 5;
+	coins += cents / 2;
+	cents %= 2;
+	coins += cents;
+	printf("%d\n", coins);
+
 	return (0);
 }
